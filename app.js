e8fbc6959078d69cbe22836d03e6c9822a139feb
vaibhav dhiman <princedhiman70@gmail.com>
2020-12-17 12:52:05 +0530

    about functions

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-function combine(input1, input2) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number')
-        result = input1 + input2;
-    else
-        result = input1.toString() + input2.toString();
-    return result;
+function add(n1, n2) {
+    return n1 + n2;
 }
-var combineAges = combine(20, 'vaibhav');
-console.log(combineAges);
+function printResult(num) {
+    console.log("result" + num);
+}
+printResult(add(23, 45));
--- app.ts
+++ app.ts
@@ -1,11 +1,17 @@
-function combine(input1: number | string, input2: number | string) {
-   let result;
-   if(typeof input1 === 'number' && typeof input2 === 'number')
-        result = input1 + input2;
-    else 
-        result = input1.toString() + input2.toString();
-    return result;
+function add(n1:number, n2:number): number {
+    return n1+n2;
 }
 
-const combineAges = combine(20,'vaibhav');
-console.log(combineAges);
+function printResult(num:number) {
+    console.log("result" + num);
+}
+
+function addHandler(n1: number, n2: number) {
+    const res = n1+n2;
+    
+}
+
+let combineValues : (a: number, b: number) => number;
+
+
+printResult(add(23,45))
