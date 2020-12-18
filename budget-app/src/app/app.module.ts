003a2ac01426e0a9494043d3141e62a2aeb6a994
vaibhav dhiman <princedhiman70@gmail.com>
2020-12-18 11:03:42 +0530

    budgest card import fixed

--- budget-app/src/app/app.module.ts
+++ budget-app/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainPageComponent } from './MainPage/MainPage.component';
 import { AddItemFormComponent } from './AddItemForm/AddItemForm.component';
 import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
+import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
 
 @NgModule({
   declarations: [			
     AppComponent,
       MainPageComponent,
       AddItemFormComponent,
-      BudgetItemListComponent
+      BudgetItemListComponent,
+      BudgetItemCardComponent
    ],
   imports: [
     BrowserModule,
