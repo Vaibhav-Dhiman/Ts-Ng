36b421d2829366ed9412c69ced31ac816191bb49
vaibhav dhiman <princedhiman70@gmail.com>
2020-12-20 18:26:58 +0530

    app completed

--- budget-app/src/app/budget-item-list/budget-item-list.component.ts
+++ budget-app/src/app/budget-item-list/budget-item-list.component.ts
@@ -3,15 +3,19 @@ import { MatDialog } from '@angular/material/dialog';
 import { BudgetItem } from 'src/shared/models/budget-item';
 import { EditItemModelComponent } from '../edit-item-model/edit-item-model.component';
 
+
 @Component({
   selector: 'app-budget-item-list',
   templateUrl: './budget-item-list.component.html',
   styleUrls: ['./budget-item-list.component.scss']
 })
+
+
 export class BudgetItemListComponent implements OnInit {
 
   @Input() budgetItems: BudgetItem[]= [];
   @Output() delete: EventEmitter<BudgetItem> = new  EventEmitter<BudgetItem>();
+  @Output() update: EventEmitter<UpdateEvent> = new EventEmitter<UpdateEvent>();
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
@@ -31,9 +35,18 @@ export class BudgetItemListComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       if(result){
         // replae the item with updated sub,itted item from the form
-        this.budgetItems[this.budgetItems.indexOf(item)] = result;
+      
+        this.update.emit({
+            old: item,
+            new: result
+        });
       }
     })
   }
 
+}
+
+export interface UpdateEvent {
+  old: BudgetItem;
+  new: BudgetItem;
 }
