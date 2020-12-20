976fa78499f046cdee79973b6bbb814cac0c72e5
vaibhav dhiman <princedhiman70@gmail.com>
2020-12-20 17:57:36 +0530

    edit done

--- budget-app/src/app/AddItemForm/AddItemForm.component.ts
+++ budget-app/src/app/AddItemForm/AddItemForm.component.ts
@@ -9,12 +9,21 @@ import { BudgetItem } from 'src/shared/models/budget-item';
 })
 export class AddItemFormComponent implements OnInit {
 
-  @Input() item: BudgetItem = new BudgetItem('', 0);
+  @Input() item: BudgetItem;
   @Output() formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
-
+  isNewItem: boolean;
   constructor() { }
 
   ngOnInit() {
+    if(this.item) {
+      // has value
+      this.isNewItem = false;
+    }
+
+    else {
+      this.isNewItem  = true;
+      this.item = new BudgetItem('',null);
+    }
   }
 
   onSubmit(form: NgForm) {
--- budget-app/src/app/edit-item-model/edit-item-model.component.ts
+++ budget-app/src/app/edit-item-model/edit-item-model.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { BudgetItem } from 'src/shared/models/budget-item';
 
 @Component({
@@ -7,15 +9,15 @@ import { BudgetItem } from 'src/shared/models/budget-item';
   styleUrls: ['./edit-item-model.component.scss']
 })
 export class EditItemModelComponent implements OnInit {
-
-  @Input() item: BudgetItem | undefined;
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<EditItemModelComponent>,
+    @Inject(MAT_DIALOG_DATA) public item: BudgetItem) { }
 
   ngOnInit() {
   }
 
   onSubmitted(updatedItem: BudgetItem) {
-    
+    this.dialogRef.close(updatedItem);
   }
 
 }
