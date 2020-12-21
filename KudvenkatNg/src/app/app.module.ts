00e3ed8061c7889aad71dee6f79e9d411ba4e84a
vaibhav dhiman <princedhiman70@gmail.com>
2020-12-21 15:16:20 +0530

    setup reactive form

--- KudvenkatNg/src/app/app.module.ts
+++ KudvenkatNg/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './employee/create-employee/create-employee.component';
@@ -14,7 +14,8 @@ import { ListEmployeeComponent } from './employee/list-employee/list-employee.co
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
